19ea46628d3e3142cd6a8b2e9bea000b97f3b91e
Pratyush Sinha <122526404+GitPratyush@users.noreply.github.com>
2025-02-14 04:15:19 +0530

    Integrated the AI feature for Shoulder Tap exercise used angle to calculate and debouncing for count optimization

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,17 @@
-
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css'
 import PushupCounter from './components/PushupCounter'
+import ShoulderTapCounter from './components/ShoulderTapCounter'
 
 function App() {
 
   return (
-    <>
-      <PushupCounter/>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/pushups" element={<PushupCounter />} />
+        <Route path="/shoulder-taps" element={<ShoulderTapCounter />} />
+      </Routes>
+    </Router>
   )
 }
 
